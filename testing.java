b5f23bf9cba81d411d6b143653bdf0086e509b08
Erik Braegelmann <ebraegelmann@gmail.com>
2014-01-13 19:22:29 -0600

    changes to testing

--- testing.java
+++ testing.java
@@ -1,21 +1,30 @@
+//MORE COMMENTS BECAUSE WHY NOT.
+
 //this line has stuff
 
 public class Testing{
+
     int a;
     int b;
     String c;
     long guid;
 
+PUTTING WORDS HERE
+
 	def magic
 		puts "what is this java thing?"
 	end
 
+SUCH WORDS
 
     public Testing(){
         a = 1;
         b = 2;
         c = "STUFF";
     }
+
+
+WOW
 
     public int getA(){
         return a;
